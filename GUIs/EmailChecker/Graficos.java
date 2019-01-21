59044c2163707cae6042561c9030b9051047d1f8
Angel Victor Juanco Müller <44986381+VokCow@users.noreply.github.com>
2019-01-21 22:42:04 +0100

    Create Graficos.java

---
+++ GUIs/EmailChecker/Graficos.java
@@ -0,0 +1,10 @@
+public class Graficos {
+
+/* This is an example of how to create graphical interfaces. We create here a sample window with an icon, a close
+ * button and a textfield. */
+    public static void main(String[] args) {
+    	//In the main we just instanciate the class VentanaPrincipal crating a ventana object
+    	VentanaPrincipal vetana = new VentanaPrincipal();
+    }
+    
+}
